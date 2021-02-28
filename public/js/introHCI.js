17878999222dd1f08b60e72d2bc4017271c735ff
Catherine Dinh <dinhcaatherine@gmail.com>
2021-02-27 17:24:04 -0800

    bug fixes

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -12,8 +12,11 @@ function initializePage() {
 
 // Function that is called when the server responds to the AJAX call
 function onServerResponse(data){
+	console.log(data.id);
     var labElementId = getLabElementId(data.id);
+    console.log(labElementId);
     var labElement = $(labElementId);
+    console.log(labElement);
     labElement.css({'background': 'yellow'});
 }
 
